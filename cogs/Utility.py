f0d109ab756b73dbe750cfc56fd59e82db3305a5
DarkKronicle <darkkronicle@gmail.com>
2020-09-30 20:03:57 -0600

    Waiting

--- cogs/Utility.py
+++ cogs/Utility.py
@@ -29,7 +29,7 @@ class Utility(commands.Cog):
                     await ctx.send("Error creating invite. Invite channel not found!")
                     return
                 
-                invite = channel.create_invite(max_age=1800)
+                invite = await channel.create_invite(max_age=1800)
                 if invite is None:
                     await ctx.send("Error creating invite. It just didn't create!")
                     return
