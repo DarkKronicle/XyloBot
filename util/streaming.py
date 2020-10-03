e69a987607fa407dd58973b0ba7fc90c7590900e
DarkKronicle <darkkronicle@gmail.com>
2020-10-03 13:41:33 -0600

    Debugging

--- util/streaming.py
+++ util/streaming.py
@@ -9,7 +9,8 @@ token = os.getenv('TWITCH_TOKEN')
 async def check_twitch_online(channel_name):
     headers = {"client-id": token, "Authorization": "Bearer 2gbdx6oar67tqtcmt49t3wpcgycthx"}
     r = requests.get(url=TWITCH_URL.format(channel_name), headers=headers)
-    data = r.json()["data"]
+    data = r.json()
+    print(data)
     if "is_live" not in data or not data["is_live"]:
         return None
     return data
