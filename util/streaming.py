905595c3b157c382232b1a5f20a7b5ad688b9f11
DarkKronicle <darkkronicle@gmail.com>
2020-10-03 13:39:29 -0600

    Headers v3

--- util/streaming.py
+++ util/streaming.py
@@ -3,14 +3,12 @@ import requests
 
 TWITCH_URL = "https://api.twitch.tv/helix/search/channels?query={}"
 
-API_HEADERS = {
-    'client-id': os.getenv('TWITCH_TOKEN'),
-    'Authorization': "Bearer 2gbdx6oar67tqtcmt49t3wpcgycthx"
-}
+token = os.getenv('TWITCH_TOKEN')
 
 
 async def check_twitch_online(channel_name):
-    r = requests.get(url=TWITCH_URL.format(channel_name), headers={"client-id": API_HEADERS['client-id'], "Authorization": API_HEADERS['Authorization']})
+    headers = {"client-id": token, "Authorization": "Bearer 2gbdx6oar67tqtcmt49t3wpcgycthx"}
+    r = requests.get(url=TWITCH_URL.format(channel_name), headers=headers)
     data = r.json()["data"]
     if "is_live" not in data or not data["is_live"]:
         return None
