dabbfa3e7e5a51d35bfe1e15cb3c18282e5105a8
DarkKronicle <darkkronicle@gmail.com>
2020-10-06 20:00:46 -0600

    Fixing limits and commands

--- cogs/data_commands.py
+++ cogs/data_commands.py
@@ -51,6 +51,14 @@ class Commands(commands.Cog):
         "birthday": "Birthday"
     }
 
+    limits = {
+        "first": 20,
+        "last": 20,
+        "school": 15,
+        "extra": 100,
+        "birthday": 10
+    }
+
     def __init__(self, bot: commands.Bot):
         self.bot = bot
 
@@ -79,7 +87,7 @@ class Commands(commands.Cog):
         else:
             message = f"`{ctx.author.name}`:"
             for f in data["fields"]:
-                message = message + f"\n-   1{self.names[f]}`: {data['fields'][f]}"
+                message = message + f"\n-   `{self.names[f]}`: {data['fields'][f]}"
 
             await ctx.send(message)
             return
@@ -112,7 +120,7 @@ class Commands(commands.Cog):
         else:
             message = f"`{user.name}`:"
             for f in data["fields"]:
-                message = message + f"\n-   {self.names[f]}: `{data['fields'][f]}`"
+                message = message + f"\n-   `{self.names[f]}: {data['fields'][f]}"
             await ctx.send(message)
             return
 
@@ -120,19 +128,14 @@ class Commands(commands.Cog):
 
     @commands.command(name="edit")
     @commands.guild_only()
-    async def edit(self, ctx: Context, *args, member: discord.Member = None):
+    async def edit(self, ctx: Context, *args):
         """
         Edit information on people. User's can only edit their own fields that do not take place in verification.
 
         Values you can use are first, last, school, extra, and birthday.
         """
         admin = check_admin(ctx, administrator=True)
-        if member is None:
-            member = ctx.author
-        else:
-            if not admin:
-                return await ctx.send("Only admin's can change other people's values!")
-            admin = True
+        member = ctx.author
         if len(args) < 2:
             await ctx.send_help('edit')
         if args[0] not in self.names:
@@ -148,7 +151,67 @@ class Commands(commands.Cog):
                     break
             if not allowed:
                 return await ctx.send("You're not allowed to change that value! Ask a staff member to change it.")
+        limit = self.limits[field]
         data = ' '.join(args[1:])
+        if len(data) > limit:
+            return await ctx.send("Content is too big! Make it smaller.")
+        if field == "birthday":
+            date_string = data
+            bformat = "%Y-%m-%d"
+            try:
+                datetime.strptime(date_string, bformat)
+            except ValueError:
+                return await ctx.send("Birthday format needs to be `YYYY-MM-DD`")
+        ask = discord.Embed(
+            title="Edit this information?",
+            description=f"{member.mention} for `{field}` to:\n\n{data}",
+            colour=discord.Colour.purple()
+        )
+        answer = await ctx.prompt(embed=ask)
+        if answer is None:
+            return await ctx.timeout()
+        if not answer:
+            return await ctx.send("Your response has been trashed.")
+
+        db = Database()
+        user_data = db.get_user(str(ctx.guild.id), str(member.id))
+        if "fields" not in user_data:
+            user_data["fields"] = {}
+        user_data["fields"][field] = data
+        db.update_user(user_data, str(member.id), str(ctx.guild.id))
+        log = cache.get_log_channel(ctx.guild)
+
+        await ctx.send(f"Edited your {field}!")
+        if log is not None:
+            log_embed = discord.Embed(
+                title="Data Changed",
+                description=f"{member.mention} changed their data.\n\nFrom `{field}` to:\n\n{data}"
+            )
+            await log.send(embed=log_embed)
+
+    @commands.command(name="editother")
+    @commands.guild_only()
+    @has_permissions(administrator=True)
+    async def editother(self, ctx: Context, member: discord.Member = None):
+        """
+        Edit information other information. You need to be an admin.
+
+        Values you can use are first, last, school, extra, and birthday.
+        """
+        if member is None:
+            return await ctx.send("You need to specify a user to edit!")
+
+        field = await ctx.ask("What field do you want to change?")
+        if field is None:
+            return await ctx.timeout()
+        if field not in self.names:
+            return await ctx.send("Not a proper field!")
+        limit = self.limits[field]
+        data = await ctx.ask("What do you want to change it to?")
+        if data is None:
+            return await ctx.timeout()
+        if len(data) > limit:
+            return await ctx.send("Content is too big! Make it smaller.")
         if field == "birthday":
             date_string = data
             bformat = "%Y-%m-%d"
