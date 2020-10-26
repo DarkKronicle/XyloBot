75de05319e3f5a0c3f75e8fc824a806a4cd00518
DarkKronicle <darkkronicle@gmail.com>
2020-10-26 14:36:23 -0600

    Fixing again

--- cogs/clip.py
+++ cogs/clip.py
@@ -32,13 +32,22 @@ class Clip(commands.Cog):
             row = con.fetchone()
         return row
 
-    @commands.group(name="clip", aliases=["c", "cp"])
-    async def clip_command(self, ctx: Context):
+    @commands.group(name="clip", aliases=["c", "cp"], invoke_without_command=True)
+    async def clip_command(self, ctx: Context, *args):
         """
         Store data inside of Xylo for future use!
         """
-        if ctx.invoked_subcommand is None:
+        if len(args) == 0:
             await ctx.send_help('clip')
+        name = ' '.join(args)
+        if not all(c in self.characters for c in name):
+            return await ctx.send("You can only only use `<a-z> <0-9> - _ ! /` in the name.")
+        if len(name) > 100:
+            return await ctx.send("The name is too long!")
+        clip = await self.get_clip(ctx.author.id, name)
+        if clip is None:
+            return await ctx.send("No clip with that name found.")
+        await ctx.send(clip['content'])
 
     characters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p",
                   "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "-", "_", "!", "/", "1", "2",
@@ -96,20 +105,6 @@ class Clip(commands.Cog):
         async with db.MaybeAcquire() as con:
             con.execute(command)
         await ctx.send("Deleted!")
-
-    @clip_command.command(name="clip")
-    async def send_clip(self, ctx: Context, *args):
-        if len(args) == 0:
-            return await ctx.send("Make sure to specify a clip!")
-        name = ' '.join(args)
-        if not all(c in self.characters for c in name):
-            return await ctx.send("You can only only use `<a-z> <0-9> - _ ! /` in the name.")
-        if len(name) > 100:
-            return await ctx.send("The name is too long!")
-        clip = await self.get_clip(ctx.author.id, name)
-        if clip is None:
-            return await ctx.send("No clip with that name found.")
-        await ctx.send(clip['content'])
 
 
 def setup(bot):
