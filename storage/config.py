a8a329eb32fbdd77d5f7fa164fcb6ea5b7f03eb8
DarkKronicle <darkkronicle@gmail.com>
2020-10-27 07:29:11 -0600

    Chang QOTD

--- storage/config.py
+++ storage/config.py
@@ -5,15 +5,10 @@ class ConfigData:
     questions = JSONReader("data/questions.json")
     join = JSONReader("data/join.json")
     idstorage = JSONReader("data/idstorage.json")
-    rolestorage = JSONReader("data/roles.json")
-    defaultsettings = JSONReader("data/guildsettings.json")
     lober = JSONReader("data/lober.json")
 
     def reload(self):
         self.questions.loadfile()
         self.join.loadfile()
-        self.autoreactions.loadfile()
         self.idstorage.loadfile()
-        self.rolestorage.loadfile()
-        self.defaultsettings.loadfile()
         self.lober.loadfile()
