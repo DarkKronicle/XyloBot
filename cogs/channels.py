b30c0d8918fd6117d69664f16a23ed7c13d7a210
DarkKronicle <darkkronicle@gmail.com>
2020-10-27 15:23:10 -0600

    Debugging

--- cogs/channels.py
+++ cogs/channels.py
@@ -88,12 +88,14 @@ class Channels(commands.Cog):
             return await ctx.send("Incorrect time format!")
         if dt.minute != 30 and dt.minute != 0:
             return await ctx.send("Time has to be divisible by 30.")
+        todel = await ctx.send("Time")
         command = "INSERT INTO qotd(guild_id, time) VALUES ({0}, $${1}$$) ON CONFLICT (guild_id) DO " \
                   "UPDATE SET " \
                   "time = $$EXCLUDED.time$$;"
         command = command.format(ctx.guild.id, time)
         async with db.MaybeAcquire() as con:
             con.execute(command)
+        await todel.delete()
         await ctx.send(f"Time updated to `{time} MST`.")
 
     @qotd_cmd.command(name="info")
