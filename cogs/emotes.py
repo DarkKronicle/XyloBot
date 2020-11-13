cb04761c80110344e59240f417244406494ca94e
DarkKronicle <darkkronicle@gmail.com>
2020-11-12 18:30:23 -0700

    Using binary decode

--- cogs/emotes.py
+++ cogs/emotes.py
@@ -13,14 +13,14 @@ class Emotes(commands.Cog):
         self.bot = bot
 
     @commands.command(name=":thumb")
-    async def thumb(self, ctx: Context, *, extra):
+    async def thumb(self, ctx: Context, *, extra: str):
         if extra is None:
             extra = "Thumb"
         embed = discord.Embed(
             description=extra,
             colour=discord.Colour.magenta()
         )
-        with open("assets/emotes/thumb.png", "r") as buffer:
+        with open("assets/emotes/thumb.png", "rb") as buffer:
             buffer.seek(0)
             file = discord.File(fp=buffer, filename="thumb.png")
         embed.set_image(url="attachment://thumb.png")
