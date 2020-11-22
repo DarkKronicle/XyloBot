db4a4a2a74ee5ea3d40097a8e6977486edcde8fc
DarkKronicle <darkkronicle@gmail.com>
2020-11-21 19:41:23 -0700

    Way more complex menu system

--- cogs/mtg.py
+++ cogs/mtg.py
@@ -36,23 +36,36 @@ class CardSearchSource(menus.ListPageSource):
         menu.embed.description = '\n'.join(pages)
         return menu.embed
 
+    async def format_card(self, menu, card):
+        menu.embed = card_embed(card)
+        return menu.embed
+
     def is_paginating(self):
         # We always want buttons so that we can view card information.
         return True
 
 
 class CardSearch(Pages):
+    """
+    A menu that consists of two parts, the list of all cards, then the in depth card view on each.
+    """
 
     def __init__(self, entries, *, per_page=15):
         super().__init__(CardSearchSource(entries, per_page=per_page))
-        self.embed = discord.Embed(colour=discord.Colour.dark_green())
+        self.embed = discord.Embed(colour=discord.Colour.magenta())
         self.entries = entries
+        self.current_card = 0
+        self.card_view = False
 
     def _skip_singles(self):
         max_pages = self._source.get_max_pages()
         if max_pages is None:
             return True
         return max_pages <= 1
+
+    async def show_page(self, page_number):
+        self.card_view = False
+        await super().show_page(page_number)
 
     @menus.button('\N{INFORMATION SOURCE}\ufe0f', position=menus.Last(3))
     async def show_help(self, payload):
@@ -66,15 +79,29 @@ class CardSearch(Pages):
         embed.set_footer(text=f'We were on page {self.current_page + 1} before this message.')
         await self.message.edit(content=None, embed=embed)
 
-    @menus.button('\N{BLACK LEFT-POINTING TRIANGLE}\ufe0f', position=menus.First(1), skip_if=_skip_singles)
+    @menus.button('\N{BLACK LEFT-POINTING TRIANGLE}\ufe0f', position=menus.First(1))
     async def go_to_previous_page(self, payload):
         """go to the previous page"""
-        await self.show_checked_page(self.current_page - 1)
+        if self.card_view:
+            await self.show_checked_page(self.current_page - 1)
+        else:
+            await self.show_card_page(self.current_card - 1)
 
-    @menus.button('\N{BLACK RIGHT-POINTING TRIANGLE}\ufe0f', position=menus.Last(0), skip_if=_skip_singles)
+    @menus.button('\N{BLACK RIGHT-POINTING TRIANGLE}\ufe0f', position=menus.Last(0))
     async def go_to_next_page(self, payload):
         """go to the next page"""
-        await self.show_checked_page(self.current_page + 1)
+        if self.card_view:
+            await self.show_checked_page(self.current_page + 1)
+        else:
+            await self.show_card_page(self.current_card + 1)
+
+    @menus.button('↩️', position=menus.Last(1))
+    async def go_current_page(self, payload):
+        """switch page view to card view"""
+        if self.card_view:
+            await self.show_checked_page(self.current_page)
+        else:
+            await self.show_card_page(self.current_card)
 
     @menus.button('📑', position=menus.Last(4))
     async def card_info(self, payload):
@@ -96,15 +123,40 @@ class CardSearch(Pages):
         else:
             number = int(msg.content)
             to_delete.append(msg)
-            if number < 1 or number > len(self.entries):
-                raise commands.BadArgument("That was too big/too small.")
-            card = self.entries[number - 1]
-            await self.message.edit(embed=card_embed(card))
+            await self.show_card_page(number - 1)
 
         try:
             await channel.delete_messages(to_delete)
         except Exception:
             pass
+
+    async def show_card_page(self, card_number):
+        max_cards = len(self.entries)
+        try:
+            if max_cards is None:
+                # If it doesn't give maximum pages, it cannot be checked
+                await self.show_card(card_number)
+            elif max_cards > card_number >= 0:
+                await self.show_card(card_number)
+        except IndexError:
+            # An error happened that can be handled, so ignore it.
+            pass
+
+    async def _get_kwargs_from_card(self, card):
+        value = await discord.utils.maybe_coroutine(self._source.format_card, self, card)
+        if isinstance(value, dict):
+            return value
+        elif isinstance(value, str):
+            return {'content': value, 'embed': None}
+        elif isinstance(value, discord.Embed):
+            return {'embed': value, 'content': None}
+
+    async def show_card(self, card_number):
+        self.card_view = True
+        card = self.entries[card_number]
+        self.current_card = card_number
+        kwargs = await self._get_kwargs_from_card(card)
+        await self.message.edit(**kwargs)
 
 
 class MagicCard(commands.Converter):
@@ -125,7 +177,11 @@ class MagicCard(commands.Converter):
                     searches = auto.data()
                     if len(searches) > 10:
                         searches = searches[:10]
-                    raise commands.BadArgument(e.error_details['details'] + f" Did you mean:\n" + "\n".join(searches))
+                    if len(searches) == 0:
+                        extra = ". Maybe use the search command to view other cards."
+                    else:
+                        extra = ". Did you mean:\n" + "\n".join(searches)
+                    raise commands.BadArgument(e.error_details['details'] + extra)
         return card
 
 
