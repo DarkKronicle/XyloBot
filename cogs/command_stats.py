525a020a183c8851d5d839526062abe3ceac9131
DarkKronicle <darkkronicle@gmail.com>
2020-11-27 22:43:23 -0700

    Per command check

--- cogs/command_stats.py
+++ cogs/command_stats.py
@@ -32,10 +32,23 @@ class CommandStats(commands.Cog):
         command = ctx.command.qualified_name
         self.data[command] += 1
 
-    @commands.group(name="stats")
-    @commands.cooldown(1, 20, commands.BucketType.user)
-    async def stats(self, ctx: Context):
+    @commands.group(name="stats", invoke_without_command=True)
+    async def stats(self, ctx: Context, *, command_name="help"):
         """View command stats!"""
+        num = self.data.get(command_name, 0)
+        if num == 0:
+            embed = discord.Embed(description=f"Hmm, looks like I don't have `{command_name}` recorded.")
+        else:
+            embed = discord.Embed(
+                description=f"I have processed a `{command_name}` a total of `{num}` times!",
+                colour=discord.Colour.magenta()
+            )
+        embed.set_footer(text="*Since November 27, 2020")
+        await ctx.send(embed=embed)
+
+    @stats.command(name="all")
+    async def all_stats(self, ctx: Context):
+        """View a sum of all commands!"""
         embed = discord.Embed(
             description=f"I have processed a total of `{sum(self.data.values())}` commands!",
             colour=discord.Colour.magenta()
@@ -52,6 +65,7 @@ class CommandStats(commands.Cog):
             description='\n'.join(f'`{k}`: {c}' for k, c in common),
             colour=discord.Colour.gold()
         )
+        embed.set_footer(text="*Since November 27, 2020")
         await ctx.send(embed=embed)
 
 
