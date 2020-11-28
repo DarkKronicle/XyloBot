fc12aed09d3c8d1d9cdb4c750b9a72382355d46a
DarkKronicle <darkkronicle@gmail.com>
2020-11-27 22:39:40 -0700

    Add usage commands

--- cogs/command_stats.py
+++ cogs/command_stats.py
@@ -1,9 +1,12 @@
 import os
-from pathlib import Path
 
+import discord
 from discord.ext import commands
+from collections import Counter
 
 import json
+
+from util.context import Context
 
 STATS_FILE = "data/stats.json"
 
@@ -16,19 +19,40 @@ class CommandStats(commands.Cog):
         with open(file=STATS_FILE, mode="a+") as f:
             f.seek(0)
             if exists:
-                self.data = json.load(f)
+                self.data = Counter(json.load(f))
             else:
-                self.data = {}
+                self.data = Counter()
 
     def cog_unload(self):
         with open(file=STATS_FILE, mode='w') as json_file:
-            json.dump(self.data, json_file, indent=4, sort_keys=True)
+            json.dump(dict(self.data), json_file, indent=4, sort_keys=True)
 
     @commands.Cog.listener()
     async def on_command(self, ctx):
         command = ctx.command.qualified_name
-        current = self.data.get(command, 0)
-        self.data[command] = current + 1
+        self.data[command] += 1
+
+    @commands.group(name="stats")
+    @commands.cooldown(1, 20, commands.BucketType.user)
+    async def stats(self, ctx: Context):
+        """View command stats!"""
+        embed = discord.Embed(
+            description=f"I have processed a total of `{sum(self.data.values())}` commands!",
+            colour=discord.Colour.magenta()
+        )
+        embed.set_footer(text="*Since November 27, 2020")
+        await ctx.send(embed=embed)
+
+    @stats.command(name="top")
+    async def top(self, ctx: Context):
+        """View the top most used commands!"""
+        common = self.data.most_common(10)
+        embed = discord.Embed(
+            title="Command Stats",
+            description='\n'.join(f'`{k}`: {c}' for k, c in common),
+            colour=discord.Colour.gold()
+        )
+        await ctx.send(embed=embed)
 
 
 def setup(bot):
